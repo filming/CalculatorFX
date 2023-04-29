f9e34b899c492dd3eea71fd0f76d663ed9b5195f
Filming <79931565+filming@users.noreply.github.com>
2023-04-29 03:39:18 -0400

    Add base calculator model

--- src/main/java/me/filming/calculatorfx/Calculator.java
+++ src/main/java/me/filming/calculatorfx/Calculator.java
@@ -1,4 +1,13 @@
 package me.filming.calculatorfx;
 
 public class Calculator {
+    private double previousAnswer;
+
+    public Calculator(){
+        previousAnswer = 0;
+    }
+
+    public double getPreviousAnswer() {
+        return previousAnswer;
+    }
 }
