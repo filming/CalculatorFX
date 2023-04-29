1e0ba00770f3a74fbe3ca0c3b01d06dcda8ddef9
Filming <79931565+filming@users.noreply.github.com>
2023-04-29 10:52:23 -0400

    Add clear, brackets, percent & division buttons

--- src/main/java/me/filming/calculatorfx/CalculatorView.java
+++ src/main/java/me/filming/calculatorfx/CalculatorView.java
@@ -1,13 +1,135 @@
 package me.filming.calculatorfx;
 
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.shape.Line;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.control.Label;
 
 public class CalculatorView extends Pane {
+    TextField outputField;
+
+    Button clearButton;
+    Button bracketsButton;
+    Button percentButton;
+    Button divideButton;
 
     public CalculatorView(){
+        // set default settings
+        setPrefSize(338, 760);
+        setStyle("-fx-background-color: #24252F");
 
-        setPrefSize(400, 600);
+        // create output display
+        outputField = new TextField();
+        outputField.setFont(Font.font("Verdana", FontWeight.BOLD, 30));
+        outputField.setPrefSize(380, 130);
+        outputField.relocate(10, 10);
+        getChildren().add(outputField);
+
+        // creating a divider
+        Rectangle divider = new Rectangle(10, 300, 380, 2);
+        Color customColour = Color.rgb(58, 58, 75);
+        divider.setFill(customColour);
+        getChildren().add(divider);
+
+        // create buttons
+        // clear button
+        StackPane clearPane = new StackPane();
+        Circle clearButtonCircle = new Circle(36, Color.rgb(60, 61, 78));
+        Label clearButtonLabel = new Label("C");
+        clearButtonLabel.setTextFill(Color.rgb(231, 121, 51));
+        clearButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        clearPane.getChildren().addAll(clearButtonCircle, clearButtonLabel);
+
+        clearButton = new Button("");
+        clearButton.setGraphic(clearPane);
+        clearButton.setShape(clearButtonCircle);
+        clearButton.setMinSize(72, 72);
+        clearButton.setMaxSize(72, 72);
+        clearButton.setBackground(Background.EMPTY);
+        clearButton.setBorder(Border.EMPTY);
+        clearButton.setPadding(Insets.EMPTY);
+        clearButton.setPickOnBounds(false);
+        clearButton.setTranslateX(10);
+        clearButton.setTranslateY(320);
+
+        // brackets button
+        StackPane bracketsPane = new StackPane();
+        Circle bracketsButtonCircle = new Circle(36, Color.rgb(60, 61, 78));
+        Label bracketsButtonLabel = new Label("(  )");
+        bracketsButtonLabel.setTextFill(Color.rgb(225, 178, 65));
+        bracketsButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        bracketsPane.getChildren().addAll(bracketsButtonCircle, bracketsButtonLabel);
+
+        bracketsButton = new Button("");
+        bracketsButton.setGraphic(bracketsPane);
+        bracketsButton.setShape(bracketsButtonCircle);
+        bracketsButton.setMinSize(72, 72);
+        bracketsButton.setMaxSize(72, 72);
+        bracketsButton.setBackground(Background.EMPTY);
+        bracketsButton.setBorder(Border.EMPTY);
+        bracketsButton.setPadding(Insets.EMPTY);
+        bracketsButton.setPickOnBounds(false);
+        bracketsButton.setTranslateX(92);
+        bracketsButton.setTranslateY(320);
+
+        // percent button
+        StackPane percentPane = new StackPane();
+        Circle percentButtonCircle = new Circle(36, Color.rgb(60, 61, 78));
+        Label percentButtonLabel = new Label("%");
+        percentButtonLabel.setTextFill(Color.rgb(225, 178, 65));
+        percentButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        percentPane.getChildren().addAll(percentButtonCircle, percentButtonLabel);
+
+        percentButton = new Button("");
+        percentButton.setGraphic(percentPane);
+        percentButton.setShape(percentButtonCircle);
+        percentButton.setMinSize(72, 72);
+        percentButton.setMaxSize(72, 72);
+        percentButton.setBackground(Background.EMPTY);
+        percentButton.setBorder(Border.EMPTY);
+        percentButton.setPadding(Insets.EMPTY);
+        percentButton.setPickOnBounds(false);
+        percentButton.setTranslateX(174);
+        percentButton.setTranslateY(320);
+
+        // divide button
+        StackPane dividePane = new StackPane();
+        Circle divideButtonCircle = new Circle(36, Color.rgb(60, 61, 78));
+        Label divideButtonLabel = new Label("÷");
+        divideButtonLabel.setTextFill(Color.rgb(225, 178, 65));
+        divideButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        dividePane.getChildren().addAll(divideButtonCircle, divideButtonLabel);
+
+        divideButton = new Button("");
+        divideButton.setGraphic(dividePane);
+        divideButton.setShape(divideButtonCircle);
+        divideButton.setMinSize(72, 72);
+        divideButton.setMaxSize(72, 72);
+        divideButton.setBackground(Background.EMPTY);
+        divideButton.setBorder(Border.EMPTY);
+        divideButton.setPadding(Insets.EMPTY);
+        divideButton.setPickOnBounds(false);
+        divideButton.setTranslateX(256); // 266
+        divideButton.setTranslateY(320);
+
+
+
+
+
+
+        getChildren().addAll(clearButton, bracketsButton, percentButton, divideButton);
     }
-
 
 }
