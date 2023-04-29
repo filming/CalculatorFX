22d19aee94072549b8dfb6e4cc0f30aae5b5107c
Filming <79931565+filming@users.noreply.github.com>
2023-04-29 03:48:08 -0400

    Add required functions for controller

--- src/main/java/me/filming/calculatorfx/CalculatorApp.java
+++ src/main/java/me/filming/calculatorfx/CalculatorApp.java
@@ -1,4 +1,14 @@
 package me.filming.calculatorfx;
 
-public class CalculatorApp {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalculatorApp extends Application {
+    public void start(Stage primaryStage) {
+        Calculator calculator = new Calculator();
+    }
+
+    public static void main (String[] args){
+        launch(args);
+    }
 }
