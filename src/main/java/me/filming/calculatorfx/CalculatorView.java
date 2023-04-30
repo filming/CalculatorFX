a7d8cd60eea65a3a90489e6339375b06dc8571c2
Filming <79931565+filming@users.noreply.github.com>
2023-04-30 02:30:42 -0400

    Add 1 btn, 2 btn, 3 btn, & addition btn

--- src/main/java/me/filming/calculatorfx/CalculatorView.java
+++ src/main/java/me/filming/calculatorfx/CalculatorView.java
@@ -301,16 +301,84 @@ public class CalculatorView extends Pane {
         subtractionButton.setTranslateY(484);
 
         // number 1 button
+        StackPane oneButtonPane = new StackPane();
+        Circle oneButtonCircle = new Circle(36, Color.rgb(46,47,61));
+        Label oneButtonLabel = new Label("1");
+        oneButtonLabel.setTextFill(Color.rgb(225, 255, 255));
+        oneButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 26));
+        oneButtonPane.getChildren().addAll(oneButtonCircle, oneButtonLabel);
 
+        oneButton = new Button("");
+        oneButton.setGraphic(oneButtonPane);
+        oneButton.setShape(oneButtonCircle);
+        oneButton.setMinSize(72, 72);
+        oneButton.setMaxSize(72, 72);
+        oneButton.setBackground(Background.EMPTY);
+        oneButton.setBorder(Border.EMPTY);
+        oneButton.setPadding(Insets.EMPTY);
+        oneButton.setPickOnBounds(false);
+        oneButton.setTranslateX(10);
+        oneButton.setTranslateY(566);
 
         // number 2 button
+        StackPane twoButtonPane = new StackPane();
+        Circle twoButtonCircle = new Circle(36, Color.rgb(46,47,61));
+        Label twoButtonLabel = new Label("2");
+        twoButtonLabel.setTextFill(Color.rgb(225, 255, 255));
+        twoButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 26));
+        twoButtonPane.getChildren().addAll(twoButtonCircle, twoButtonLabel);
 
+        twoButton = new Button("");
+        twoButton.setGraphic(twoButtonPane);
+        twoButton.setShape(twoButtonCircle);
+        twoButton.setMinSize(72, 72);
+        twoButton.setMaxSize(72, 72);
+        twoButton.setBackground(Background.EMPTY);
+        twoButton.setBorder(Border.EMPTY);
+        twoButton.setPadding(Insets.EMPTY);
+        twoButton.setPickOnBounds(false);
+        twoButton.setTranslateX(92);
+        twoButton.setTranslateY(566);
 
         // number 3 button
+        StackPane threeButtonPane = new StackPane();
+        Circle threeButtonCircle = new Circle(36, Color.rgb(46,47,61));
+        Label threeButtonLabel = new Label("3");
+        threeButtonLabel.setTextFill(Color.rgb(225, 255, 255));
+        threeButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 26));
+        threeButtonPane.getChildren().addAll(threeButtonCircle, threeButtonLabel);
 
+        threeButton = new Button("");
+        threeButton.setGraphic(threeButtonPane);
+        threeButton.setShape(threeButtonCircle);
+        threeButton.setMinSize(72, 72);
+        threeButton.setMaxSize(72, 72);
+        threeButton.setBackground(Background.EMPTY);
+        threeButton.setBorder(Border.EMPTY);
+        threeButton.setPadding(Insets.EMPTY);
+        threeButton.setPickOnBounds(false);
+        threeButton.setTranslateX(174);
+        threeButton.setTranslateY(566);
 
         // addition button
+        StackPane additionButtonPane = new StackPane();
+        Circle additionButtonCircle = new Circle(36, Color.rgb(46,47,61));
+        Label additionButtonLabel = new Label("+");
+        additionButtonLabel.setTextFill(Color.rgb(225, 178, 65));
+        additionButtonLabel.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+        additionButtonPane.getChildren().addAll(additionButtonCircle, additionButtonLabel);
 
+        additionButton = new Button("");
+        additionButton.setGraphic(additionButtonPane);
+        additionButton.setShape(additionButtonCircle);
+        additionButton.setMinSize(72, 72);
+        additionButton.setMaxSize(72, 72);
+        additionButton.setBackground(Background.EMPTY);
+        additionButton.setBorder(Border.EMPTY);
+        additionButton.setPadding(Insets.EMPTY);
+        additionButton.setPickOnBounds(false);
+        additionButton.setTranslateX(256);
+        additionButton.setTranslateY(566);
 
         // sign button
 
@@ -331,7 +399,8 @@ public class CalculatorView extends Pane {
         getChildren().addAll(
                 clearButton, bracketsButton, percentButton, divideButton,
                 sevenButton, eightButton, nineButton, multiplyButton,
-                fourButton, fiveButton, sixButton, subtractionButton
+                fourButton, fiveButton, sixButton, subtractionButton,
+                oneButton, twoButton, threeButton, additionButton
         );
     }
 
