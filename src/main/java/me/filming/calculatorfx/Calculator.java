fc17da7f03c63955702cbba9692a2599919f8ef0
Filming <79931565+filming@users.noreply.github.com>
2023-05-01 09:09:46 -0400

    Add number saving code to model

--- src/main/java/me/filming/calculatorfx/Calculator.java
+++ src/main/java/me/filming/calculatorfx/Calculator.java
@@ -1,13 +1,22 @@
 package me.filming.calculatorfx;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Calculator {
     private double previousAnswer;
+    private ArrayList<String> currentInputs;
 
     public Calculator(){
         previousAnswer = 0;
+        currentInputs = new ArrayList();
     }
 
     public double getPreviousAnswer() {
         return previousAnswer;
+    }
+
+    public void addInput(String newInput){
+        currentInputs.add(newInput);
     }
 }
